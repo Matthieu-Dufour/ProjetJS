0600971c32fba98740ac9c66032bc590c95a334c
Matthieu Dufour <matthieu.dufour3@etu.univ-lorraine.fr>
2019-03-07 23:56:27 +0100

    style

--- script.js
+++ script.js
@@ -3,84 +3,102 @@
 // Component qui génère la table pour afficher les artistes
 Vue.component('tablelistartists', {
     template:
-    `
+        `
     <div v-if="artistsdata != ''">
         <table id="mytable" class="table table-dark">
             <thead>
                 <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
                 <th scope="col">Nom</th>
                 <th scope="col">Type</th>
                 <th scope="col">Score</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
             <tr v-for="artist in artistsdata">
+                    <td></td><td></td>
                     <td>{{ artist.name }}</td>
                     <td>{{ artist.type }}</td>
                     <td>{{ artist.score }}</td>
+                    <td></td><td></td>
                 </tr>
             </tbody>
         </table>
     </div>
-    ` 
+    `
     ,
 
-    props:['artistsdata'],
+    props: ['artistsdata'],
 })
 
 // Component qui génère la table pour afficher les chansons
 Vue.component('tablelistrecordings', {
     template:
-    `
+        `
     <div v-if="recordingsdata != ''">
         <table id="mytable" class="table table-dark">
             <thead>
                 <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
                 <th scope="col">Nom</th>
                 <th scope="col">deux</th>
                 <th scope="col">Score</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
             <tr v-for="recording in recordingsdata">
+                    <td></td><td></td>
                     <td>{{ recording.title }}</td>
                     <td>{{ recording.releases.date }}</td>
                     <td>{{ recording.score }}</td>
+                    <td></td><td></td>
                 </tr>
             </tbody>
         </table>
     </div>
-    ` 
+    `
     ,
 
-    props:['recordingsdata'],
+    props: ['recordingsdata'],
 })
 
 
 // Component qui génère la table pour afficher les albums (cdstubs)
 Vue.component('tablelistcdstubs', {
     template:
-    `
+        `
     <div v-if="cdstubsdata != ''">
         <table id="mytable" class="table table-dark">
             <thead>
                 <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
                 <th scope="col">Nom</th>
                 <th scope="col">Artiste</th>
                 <th scope="col">Score</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
             <tr v-for="cdstub in cdstubsdata">
+                    <td></td><td></td>
                     <td>{{ cdstub.title }}</td>
                     <td>{{ cdstub.artist }}</td>
                     <td>{{ cdstub.score }}</td>
+                    <td></td><td></td>
                 </tr>
             </tbody>
         </table>
     </div>
-    ` 
+    `
     ,
 
-    props:['cdstubsdata'],
+    props: ['cdstubsdata'],
 })
