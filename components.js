57c9c6a96b0c4d970f50d35acf806b992579418d
Matthieu Dufour <matthieu.dufour3@etu.univ-lorraine.fr>
2019-03-12 21:02:09 +0100

    corrections

--- components.js
+++ components.js
@@ -1,10 +1,19 @@
-/* components pour music brainz */
+/* components pour l'application music brainz */
+
+// Component du spinner de chargement
+Vue.component('spinner', {
+    template:
+    `
+        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    `
+})
 
 // Component qui génère la table pour afficher les artistes
 Vue.component('tablelistartists', {
     template:
-        `
+    `
     <div v-if="artistsdata != ''">
+
         <table id="mytable" class="table table-hover table-dark">
             <thead>
                 <tr>
@@ -13,23 +22,22 @@ Vue.component('tablelistartists', {
                 <th scope="col">Nom</th>
                 <th scope="col">Type</th>
                 <th scope="col">Score</th>
-                <th scope="col">Voir</th>
+                <th scope="col"></th>
                 <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="artist in artistsdata">
-                
+                <tr v-for="artist in artistsdata" v-on:click="$emit('showartistevent', artist.id)">
                     <td></td><td></td>
                     <td>{{ artist.name }}</td>
                     <td>{{ artist.type }}</td>
                     <td>{{ artist.score }}</td>
-                    <td><button id="buttonShowArtist" type="button" class="btn" v-on:click="$emit('showartistevent', artist.id)">Voir</button></td>
                     <td></td>
-
+                    <td></td>
                 </tr>
             </tbody>
         </table>
+
     </div>
     `
     ,
@@ -42,33 +50,26 @@ Vue.component('tablelistalbums', {
     template:
         `
     <div v-if="artistinfo != ''">
-            <div id="stripArtistName" class="d-flex justify-content-center"><h2 id="artistName">{{ artistinfo.name }}</h2></div>
-            
+        <div id="stripArtistName" class="d-flex justify-content-center"><h2 id="artistName">{{ artistinfo.name }}</h2></div>
             <div class="container">
+            <h3>Albums de {{ artistinfo.name }} : </h3>
 
-            <button id="backButton" type="button" class="btn" v-on:click="$emit('backtosearchevent')">Retour</button>
-
-            <h3>Liste des albums</h3>
-            
             <table id="albumsTable" class="table table-hover table-dark">
             <thead>
                 <tr>
                     <th scope="col">Titre</th>
                     <th scope="col">Date</th>
-                    <th scope="col">Tracklist</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="release in artistinfo.releases">
+                <tr v-for="release in artistinfo.releases" v-on:click="$emit('showalbumevent', release.id)">
                     <td>{{ release.title }}</td>
                     <td>{{ release.date }}</td>
-                    <td><button id="buttonShowArtist" type="button" class="btn" v-on:click="$emit('showalbumevent', release.id)">Tracklist</button></td>
                 </tr>
             </tbody>
-        </table>
+            </table>
 
         </div>
-
     </div>
     `
     ,
@@ -87,22 +88,23 @@ Vue.component('tablelistrecordings', {
         <table id="mytable" class="table table-hover table-dark">
             <thead>
                 <tr>
-                <th scope="col"></th>
-                <th scope="col"></th>
-                <th scope="col">Nom</th>
-                <th scope="col">Date</th>
-                <th scope="col">Score</th>
-                <th scope="col"></th>
-                <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col">Nom</th>
+                    <th scope="col" >Artiste</th>
+                    <th scope="col" >Album</th>
+                    <th scope="col">Score</th>
+                    <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
-            <tr v-for="recording in recordingsdata">
+                <tr v-for="recording in recordingsdata" id="notClickable">
                     <td></td><td></td>
                     <td>{{ recording.title }}</td>
-                    <td>{{ recording.releases.date }}</td>
+                    <td v-on:click="$emit('showartistevent', recording['artist-credit'][0].artist.id )"> <a id="nomArtistAlbumPage" >{{ recording['artist-credit'][0].artist.name }}</a></td>
+                    <td v-on:click="$emit('showalbumevent', recording['releases'][0].id)"><a id="nomArtistAlbumPage" >{{ recording.releases[0].title.trim() }}</a></td>
                     <td>{{ recording.score }}</td>
-                    <td></td><td></td>
+                    <td></td>
                 </tr>
             </tbody>
         </table>
@@ -114,7 +116,7 @@ Vue.component('tablelistrecordings', {
 })
 
 
-// Component qui génère la table pour afficher les albums d'un artiste
+// Component qui génère la table pour afficher les chansons d'un albums
 Vue.component('tablelistalbumrecordings', {
     template:
         `
@@ -123,9 +125,7 @@ Vue.component('tablelistalbumrecordings', {
             
             <div class="container">
 
-            <button id="backButton" type="button" class="btn" v-on:click="$emit('backtosearchevent')">Retour</button>
-
-            <h3>Liste des titres dans l'album {{ albuminfo.title }} de {{ albuminfo['artist-credit'][0].artist.name }} <button id="buttonShowArtist" type="button" class="btn" v-on:click="$emit('showartistevent', albuminfo['artist-credit'][0].artist.id)">Voir</button></h3>
+            <h3>Tracklist de l'album {{ albuminfo.title }} de <span id="nomArtistAlbumPage" v-on:click="$emit('showartistevent', albuminfo['artist-credit'][0].artist.id)"> {{ albuminfo['artist-credit'][0].artist.name }} </span></h3>
             
             <table id="albumsTable" class="table table-hover table-dark">
             <thead>
@@ -137,20 +137,28 @@ Vue.component('tablelistalbumrecordings', {
             </thead>
             <tbody>
                 <tr v-for="record in albuminfo.media[0].tracks">
-                            <td>{{ record.number }}</td>
-                            <td>{{ record.title }}</td>
-                            <td>{{ record.length }} ms</td>
+                    <td>{{ record.number }}</td>
+                    <td>{{ record.title }}</td>
+                    <td>{{ record.length | convertTime }} min</td>
                 </tr>
             </tbody>
         </table>
-
         </div>
-
     </div>
     `
     ,
 
     props: ['albuminfo'],
+    filters: {
+
+        //Filtre pour convertir le temps des tracks de millisecondes à secondes et minutes
+        convertTime: function (value) {
+            var date = new Date(value)
+            var m = date.getMinutes()
+            var s = date.getSeconds()
+            return m + "\'" + s + "\'\'"
+        }
+    },
 })
 
 // Component qui génère la table pour afficher les albums (release)
@@ -163,20 +171,22 @@ Vue.component('tablelistrealeases', {
                 <tr>
                 <th scope="col"></th>
                 <th scope="col"></th>
-                <th scope="col">Nom</th>
+                <th scope="col">Titre</th>
                 <th scope="col">Artiste</th>
+                <th scope="col">Date</th>
                 <th scope="col">Score</th>
                 <th scope="col"></th>
                 <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
-            <tr v-for="release in releasesdata">
+            <tr v-for="release in releasesdata" v-on:click="$emit('showalbumevent', release.id)">
                     <td></td><td></td>
                     <td>{{ release.title }}</td>
-                    <td> release</td>
+                    <td> {{ release['artist-credit'][0].artist.name }}</td>
+                    <td>{{ release.date }}</td>
                     <td>{{ release.score }}</td>
-                    <td><button id="buttonShowArtist" type="button" class="btn" v-on:click="$emit('showalbumevent', release.id)">Voir</button></td>
+                    <td></td>
                     <td></td>
                 </tr>
             </tbody>
